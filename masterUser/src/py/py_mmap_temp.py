ad86249839cae8783cb799757d8a8335a0243b4d
wonjsohn <wonjsohn@gmail.com>
2015-07-13 16:44:17 -0700

    py_mmap changed

--- masterUser/src/py/py_mmap_temp.py
+++ masterUser/src/py/py_mmap_temp.py
@@ -97,16 +97,7 @@ if __name__ == "__main__":
     import WaveGen
     import datetime
     from time import gmtime, strftime
-    
-#    from Utilities import *
-    import sys
-    sys.path.insert(0,'/Network/Servers/duerer/Volumes/duerer/guest/nerf-verilog/source/py/multC_tester')
     from Utilities import *
-    from config_test import *
-    from MVC_MainGUI import MultiXemScheduler
-    from C_XemScheduler import SingleXemTester # Controller in MVC
-    from M_Fpga import SomeFpga # Model in MVC
-    from V_Display import View, ViewChannel,  CtrlChannel # Viewer in MVC
     
     #from generate_sin import gen as gen_sin
     #sys.path.append('/home/eric/nerf_verilog_eric/source/py')
@@ -118,32 +109,21 @@ if __name__ == "__main__":
     elif arch == "64bit":
         from opalkelly_64bit import ok
     
-#    BIT_FILE = HOMEPATH + "prog/masterUser/src/one_joint_robot_xem6010.bit"
-#    BIT_FILE2 = HOMEPATH + "prog/masterUser/src/one_joint_robot2_xem6010.bit"
-    BIT_FILE_BS = HOMEPATH + "prog/masterUser/src/rack_test_xem6010.bit"     # BS: Biceps Sensory
-#    BIT_FILE_TS = HOMEPATH + "prog/masterUser/src/rack_test_xem6010_2.bit" # TS: Triceps Sensory 
-    BIT_FILE_BM = HOMEPATH + "prog/masterUser/src/rack_emg_xem6010.bit"      # BM: Biceps Motor
-#    BIT_FILE_TM = HOMEPATH + "prog/masterUser/src/rack_emg_xem6010_2.bit"    # TM: Triceps Motor
+    BIT_FILE = HOMEPATH + "prog/masterUser/src/one_joint_robot_xem6010.bit"
+    BIT_FILE2 = HOMEPATH + "prog/masterUser/src/one_joint_robot2_xem6010.bit"
     
+    print BIT_FILE + "is here"
+    print BIT_FILE2
+    #assert os.path.exists(BIT_FILE.encode('utf-8')), ".bit file NOT found!"
     
-    print BIT_FILE_BS + " is here"
-#    print BIT_FILE_TS   + " is here"
-    print BIT_FILE_BM  + " is here"
-#    print BIT_FILE_TM  + " is here"
-    assert os.path.exists(BIT_FILE_BS.encode('utf-8')), ".bit file NOT found!"
-#    assert os.path.exists(BIT_FILE_TS.encode('utf-8')), ".bit file NOT found!"
-    assert os.path.exists(BIT_FILE_BM.encode('utf-8')), ".bit file NOT found!"
-#    assert os.path.exists(BIT_FILE_TM.encode('utf-8')), ".bit file NOT found!"
+    #assert os.path.exists(BIT_FILE2.encode('utf-8')), ".bit file NOT found!"
     
     #BIT_FILE2 = "/home/eric/robot_simulator/prog/masterUser/src/sinewave_sender_xem6010.bit"
     
-#    from Fpga import FpgaDevice
-    from M_Fpga import SomeFpga # Model in MVC
-#    BIT_FILE_LIST = [BIT_FILE_BS, BIT_FILE_BM,  BIT_FILE_TS, BIT_FILE_TM]
-    BIT_FILE_LIST = [BIT_FILE_BS,   BIT_FILE_BM]
+    from Fpga import FpgaDevice
+    BIT_FILE_LIST = [BIT_FILE,  BIT_FILE2]
     #BIT_FILE_LIST = [BIT_FILE]
     
-    print BIT_FILE_LIST
     ## 
     rawxem = ok.FrontPanel()
     #count = 2
@@ -169,23 +149,22 @@ if __name__ == "__main__":
     """ working board choices: 1137000222  /  1201000216/  12320003RM / 12320003RN """    
     """By 012514: '0000000550', '000000053X', '000000054K', '11160001CJ', '12320003RN', '12430003T2'"""
     
-#    nerfModel = FpgaDevice("12320003RN",  BIT_FILE_LIST[0])  # BS 
-    nerfModel = SomeFpga(NUM_NEURON, SAMPLING_RATE, '12320003RN')
-    print "serial = 12320003RN  \n"
-#    nerfModel.onClkRate(sim_speed) # 1x realtime
+    nerfModel = FpgaDevice("12430003T2",  BIT_FILE_LIST[0])
+    print "serial = 12430003T2  \n"
+    nerfModel.onClkRate(sim_speed) # 1x realtime
     xemList.append(nerfModel)
-    nerfModel = SomeFpga(NUM_NEURON, SAMPLING_RATE, '12430003T2')   # BM
-    print "serial = 12430003T2 \n"
-#    nerfModel.onClkRate(sim_speed) # 1x realtime
+    nerfModel = FpgaDevice("11160001CJ",  BIT_FILE_LIST[1])  # transcortical part
+    print "serial = 11160001CJ : transcortical \n"
+    nerfModel.onClkRate(sim_speed) # 1x realtime
     xemList.append(nerfModel)
     
-    nerfModel = SomeFpga(NUM_NEURON, SAMPLING_RATE, '000000054K')  # TS
-    print "serial = 000000054K  \n"
-#    nerfModel.onClkRate(sim_speed) # 1x realtime
-    xemList.append(nerfModel)
-    nerfModel =SomeFpga(NUM_NEURON, SAMPLING_RATE, '0000000550')  # TM
+    nerfModel = FpgaDevice("0000000550",  BIT_FILE_LIST[0])
     print "serial = 0000000550  \n"
-#    nerfModel.onClkRate(sim_speed) # 1x realtime
+    nerfModel.onClkRate(sim_speed) # 1x realtime
+    xemList.append(nerfModel)
+    nerfModel = FpgaDevice("000000053X",  BIT_FILE_LIST[1])  # transcortical part
+    print "serial = 000000053X : transcortical  \n"
+    nerfModel.onClkRate(sim_speed) # 1x realtime
     xemList.append(nerfModel)
     
     
@@ -227,30 +206,13 @@ if __name__ == "__main__":
     desiredLen = 1.0;
     coeff = -10.0;
     currentLen = 1.3;
-#    TRIG_EVENT_GAIN = 1;
-#    TRIG_EVENT_LCE = 8;
-#    TRIG_EVENT_VEL = 9;
-#    TRIG_EVENT_GAIN_SYN = 6;
-
-
-    """ sensory triggers """
-    TRIG_EVENT_SENSORY_IA_GAIN = 1; #float32
-    TRIG_EVENT_SENSORY_II_GAIN = 10; #float32
-    TRIG_EVENT_SENSORY_LCE = 9;    #float32
-    
-    """ motor triggers """
-    TRIG_EVENT_MOTOR_IA_GAIN = 3; #float32
-    TRIG_EVENT_MOTOR_CN_GAIN = 10; #float32
-    TRIG_EVENT_MOTOR_II_GAIN = 11;  #float32
-    TRIG_EVENT_MOTOR_LCE_VEL = 9;  # float32
-#    TRIG_EVENT_GAIN_SYN = 6;
-    
+    TRIG_EVENT_GAIN = 1;
+    TRIG_EVENT_LCE = 8;
+    TRIG_EVENT_VEL = 9;
+    TRIG_EVENT_GAIN_SYN = 6;
     deltaLen=0.0;
     moment_arm = 0.01;
-    BUTTON_RESET = 0
-    BUTTON_RESET_SIM = 2; # from 1
-    BUTTON_INPUT_FROM_TRIGGER = 1
-
+    BUTTON_RESET_SIM = 1;
     
     """ SEND SINEWAVE ALWAYS  """
     #pipeInData = gen_sin(F = 2.0, BIAS = 105.0, AMP = 75.0,  T = 2.0)  #1 Hz realtime. , T matters. T=2 is default. 
@@ -269,20 +231,17 @@ if __name__ == "__main__":
     xemList[2].SendButton(False, BUTTON_RESET_SIM)
     xemList[3].SendButton(False, BUTTON_RESET_SIM)
        
-#    GAIN_SYN = ConvertType(1.0, fromType = 'f', toType = 'I')
-#    xemList[0].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # send to FPGA (biceps)
-#    xemList[1].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # 
-#    xemList[2].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # send to FPGA (triceps)
-#    xemList[3].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # 
+    GAIN_SYN = ConvertType(1.0, fromType = 'f', toType = 'I')
+    xemList[0].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # send to FPGA (biceps)
+    xemList[1].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # 
+    xemList[2].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # send to FPGA (triceps)
+    xemList[3].SendPara(GAIN_SYN, trigEvent = TRIG_EVENT_GAIN_SYN)   # 
     
-#    GAIN = ConvertType(30.0, fromType = 'f', toType = 'I')
-#    xemList[0].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # send to FPGA (biceps)
-#    #xemList[1].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # 
-#    xemList[2].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # send to FPGA (triceps)
-#    #xemList[3].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # 
-    
-    
-  
+    GAIN = ConvertType(30.0, fromType = 'f', toType = 'I')
+    xemList[0].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # send to FPGA (biceps)
+    #xemList[1].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # 
+    xemList[2].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # send to FPGA (triceps)
+    #xemList[3].SendPara(GAIN, trigEvent = TRIG_EVENT_GAIN)   # 
     
     xemList[0].SendButton(True, BUTTON_RESET_SIM)
     xemList[1].SendButton(True, BUTTON_RESET_SIM)
@@ -293,15 +252,10 @@ if __name__ == "__main__":
     xemList[2].SendButton(False, BUTTON_RESET_SIM)
     xemList[3].SendButton(False, BUTTON_RESET_SIM)
     
-    bitVal_bic = convertType(1.0, fromType = 'f', toType = 'I')
-    bitVal_tri = convertType(1.0, fromType = 'f', toType = 'I')
-    
-
-    xemList[0].SendPara(bitVal_bic, trigEvent = TRIG_EVENT_SENSORY_LCE)   #BS
-    xemList[2].SendPara(bitVal_tri, trigEvent = TRIG_EVENT_SENSORY_LCE)   #  TS
-    
-    xemList[1].SendMultiPara(bitVal1 = bitVal_bic, bitVal2=0,  trigEvent =TRIG_EVENT_MOTOR_LCE_VEL) #BS
-    xemList[3].SendMultiPara(bitVal1 = bitVal_tri, bitVal2=0,  trigEvent = TRIG_EVENT_MOTOR_LCE_VEL) #TS
+    bitVal_bic = ConvertType(1.0, fromType = 'f', toType = 'I')
+    bitVal_tri = ConvertType(1.0, fromType = 'f', toType = 'I')
+    xemList[0].SendPara(bitVal_bic, trigEvent = TRIG_EVENT_LCE)   # send to FPGA (biceps)
+    xemList[2].SendPara(bitVal_tri, trigEvent = TRIG_EVENT_LCE)   # send to FPGA (triceps)
     
     """ log data writing etc """ 
     writeFilePath='../logfile'
@@ -310,14 +264,11 @@ if __name__ == "__main__":
       
     """ running loop """   
     i = 0
-    
-
-    """ xemList 0: BS, 1:BM, 2:TS, 3:TM """
     while (1):     
 
-        biceps_force = xemList[1].ReadFPGA(0x32, 'float32') # 0x30: force
+        biceps_force = xemList[0].ReadFPGA(0x30, 'float32') # 0x30: force
         #biceps_emg = xemList[0].ReadFPGA(0x26, 'int32') # 0x26: i_emg
-        triceps_force = xemList[3].ReadFPGA(0x32, 'float32') # 0x30: force 
+        triceps_force = xemList[2].ReadFPGA(0x30, 'float32') # 0x30: force 
         
         # TODO Data logging from FPGA (e.g. emg data)
         
@@ -368,15 +319,16 @@ if __name__ == "__main__":
         print 'currentLen_bic = %f,  currentLen_tri = %f' % (currentLen_bic, currentLen_tri)
         
         
-        bitVal_bic = convertType(currentLen_bic, fromType = 'f', toType = 'I')
-        bitVal_tri = convertType(currentLen_tri, fromType = 'f', toType = 'I')
-        bitVal_vel_bic = convertType(currentVel_bic, fromType = 'f', toType = 'I')
-        bitVal_vel_tri = convertType(currentVel_tri, fromType = 'f', toType = 'I')
+        bitVal_bic = ConvertType(currentLen_bic, fromType = 'f', toType = 'I')
+        bitVal_tri = ConvertType(currentLen_tri, fromType = 'f', toType = 'I')
+        bitVal_vel_bic = ConvertType(currentVel_bic, fromType = 'f', toType = 'I')
+        bitVal_vel_tri = ConvertType(currentVel_tri, fromType = 'f', toType = 'I')
 
-        xemList[0].SendPara(bitVal_bic, trigEvent = TRIG_EVENT_SENSORY_LCE)   # send to BS
-        xemList[2].SendPara(bitVal_tri, trigEvent = TRIG_EVENT_SENSORY_LCE)   # send to TS
-        xemList[1].SendMultiPara(bitVal1 =bitVal_bic,  bitVal2=bitVal_vel_bic, trigEvent = TRIG_EVENT_MOTOR_LCE_VEL)   # send to BM
-        xemList[3].SendMultiPara(bitVal1 =bitVal_tri,  bitVal2=bitVal_vel_tri, trigEvent = TRIG_EVENT_MOTOR_LCE_VEL)   # send to TM
+        xemList[0].SendPara(bitVal_bic, trigEvent = TRIG_EVENT_LCE)   # send to FPGA (biceps)
+        xemList[2].SendPara(bitVal_tri, trigEvent = TRIG_EVENT_LCE)   # send to FPGA (triceps)
+        xemList[0].SendPara(bitVal_vel_bic, trigEvent = TRIG_EVENT_VEL)   # send to FPGA (biceps)
+        xemList[2].SendPara(bitVal_vel_tri, trigEvent = TRIG_EVENT_VEL)   # send to FPGA (triceps)
+        
 
         
         #time.sleep(0.01)
